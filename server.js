695da5488a3d0e1e85a3ac7992fb8bcb9c9f4f5c
br00kd0wnt0n <brookdownton@me.com>
2025-05-08 12:59:23 -0400

    Update CORS for Railway production domain

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ app.use((req, res, next) => {
 // Middleware
 app.use(cors({
     origin: process.env.NODE_ENV === 'production' 
-        ? ['https://netflix-house-prototype.up.railway.app', 'https://www.netflixhouse.com'] 
+        ? ['https://nfhaerialproto-production.up.railway.app', 'https://www.netflixhouse.com'] 
         : 'http://localhost:3000',
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization']
